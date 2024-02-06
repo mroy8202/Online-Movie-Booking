2dc5ccc33a247fc17211a385bc1849c45274e826
mritunjayRoy <mroy8202@gmail.com>
2024-02-06 20:59:43 +0530

    vite replaced with CRA

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const routes = require("./routes/routes");
 
@@ -17,6 +18,12 @@ database.connect();
 // middlewares
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true,
+    })
+);
 
 app.use(fileUpload({
     useTempFiles : true,
